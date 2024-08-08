21f8cd933e69d4292dba87c6fa3819235875d5ce
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2024-08-07 21:47:02 -1000

    AncestryTraitSelect works now

---
+++ src/components/CharacterCreator/ancestryTraitSelect.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type iTrait from '@/interfaces/iTraits';
+
+const props = defineProps<{
+  traits: iTrait[];
+}>();
+
+const emit = defineEmits<{
+  (e: 'setAncestryTrait', newAncestryTraitName: string): string;
+}>();
+
+let options = props.traits.map((trt) => trt.name);
+
+function onChange(event: any) {
+  let input = event.target.value;
+  console.log(props.traits);
+  emit('setAncestryTrait', input);
+}
+</script>
+
+<template>
+  <select :onchange="onChange">
+    <option selected disabled>Select Ancestry Trait</option>
+    <option v-for="opt in options" :value="opt" :key="opt">
+      {{ opt }}
+    </option>
+  </select>
+</template>
