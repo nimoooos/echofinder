1f212af0b4dc132e6fe9c6e89cba7d17872aa117
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2024-08-09 16:37:25 -1000

    feat: started working on featureCard that can be displayed in character sheet

---
+++ src/views/featureCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+//TODO: set up props to receive trait data
+//TODO: put it into a format that's easily readable by the template
+//TODO: dynamic style based on feature type
+</script>
+
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>
+          <h1>Feature name</h1>
+          <p>tag1, tag2, tag3</p>
+        </th>
+      </tr>
+      <p>rules text</p>
+      <tr>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>
