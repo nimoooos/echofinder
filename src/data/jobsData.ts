63e41dc7fbc528be3eeabdcd213c193adef37bc2
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2024-08-17 01:20:07 -1000

    fix: correct number of line breaks

--- src/data/jobsData.ts
+++ src/data/jobsData.ts
@@ -69,7 +69,7 @@ export const jobsData: iJob[] = [
       {
         name: 'Day/Night Cycle',
         type: 'Trait',
-        text: "You have two special resources - Sun and Moon - that each start at 0. You can only have one of these resources at a time, and they reset to 0 at the end of each combat scene. \n Sun and Moon have the following effects: \n\t⬦ Sun: For each Sun you have, your AOE spells and artifact weapons gain +1 to their AOEs (Line AOEs have their width increased by +1, instead); however, you must spend +1 MP each time you use one of these spells or weapons. If you can't, you immediately lose all Sun. \n\t⬦ Moon: For each Moon you have, your Scope and Range with artifact weapons and spells that have a Range increase by +4; however, you take 1 Stress each time you target something with a spell or artifact weapon attack beyond your normal Scope or the weapon's Range.",
+        text: "You have two special resources - Sun and Moon - that each start at 0. You can only have one of these resources at a time, and they reset to 0 at the end of each combat scene.\n\nSun and Moon have the following effects: \n\t⬦ Sun: For each Sun you have, your AOE spells and artifact weapons gain +1 to their AOEs (Line AOEs have their width increased by +1, instead); however, you must spend +1 MP each time you use one of these spells or weapons. If you can't, you immediately lose all Sun. \n\t⬦ Moon: For each Moon you have, your Scope and Range with artifact weapons and spells that have a Range increase by +4; however, you take 1 Stress each time you target something with a spell or artifact weapon attack beyond your normal Scope or the weapon's Range.",
         gainedActions: [],
         actionData: undefined,
       },
